3a1dca2cd0d3c5b2bafbd96778f0559a6b3d79b6
kamrankausar <kausar.kamran@gmail.com>
2016-08-06 01:32:11 +0530

    Create sumprimes.py
    This script sum all prime number in a give integer array

---
+++ sumprimes.py
@@ -0,0 +1,13 @@
+def sumprimes(l):
+         sum = 0 
+         for i in l:
+             if i > 0:
+                 prime = True
+                 for j in range(2,i):
+                     if (i%j == 0):
+                        prime = False
+                 if prime:
+                    sum = sum + i
+                                   
+         return(sum)
+
