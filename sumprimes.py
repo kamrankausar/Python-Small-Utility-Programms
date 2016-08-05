c2f97152fac1a2f96a5157464e8ff8f9ecbe1725
kamrankausar <kausar.kamran@gmail.com>
2016-08-06 01:37:40 +0530

    Update sumprimes.py

--- sumprimes.py
+++ sumprimes.py
@@ -1,7 +1,7 @@
 def sumprimes(l):
          sum = 0 
          for i in l:
-             if i > 0:
+             if i > 1:
                  prime = True
                  for j in range(2,i):
                      if (i%j == 0):
