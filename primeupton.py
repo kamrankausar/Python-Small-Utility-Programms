805f85283cb1415194213660ae668317ac8c53b5
kamrankausar <kausar.kamran@gmail.com>
2016-08-13 08:17:34 +0530

    Create primeupton.py

---
+++ primeupton.py
@@ -0,0 +1,20 @@
+# Python Script to find is any given number is prime or not 
+# First we find factor of that number Script of Factor 
+def fac(n):
+  res = []
+  for i in range(1,n+1):
+    if n%i == 0:
+      res.append(i)
+#Script to check prime or not
+def prime(n):
+    return(fac(n) == [1,n])
+  
+#Script print all prime number upto n
+def prime_list(n):
+  pri_list = []
+  for i in range(1,n+1):
+    val = prime(i)
+    pri_list.append(i)
+  return(pri_list)
+
+  
